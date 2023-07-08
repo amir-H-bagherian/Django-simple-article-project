17c330175ddefc5566486c488d142732cd30e8da
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-07-08 21:28:37 +0330

    view routes added to urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from .views import ArticleCreateView, ArticleDetailView, ArticleListView
 
 urlpatterns = [
+    path('create/', ArticleCreateView.as_view(), name='article_create'),
+    path('list/', ArticleListView.as_view(), name='article_create'),
+    path('detail/<int:id>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
