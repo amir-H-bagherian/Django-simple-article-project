f82cb321a19375706683d14f5de89d051006b6ab
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-07-08 21:08:19 +0330

    ArticleCreateView class added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib import messages
+from django.shortcuts import redirect
 
+from .forms import ArticleCreateForm
 # Create your views here.
+
+
+class ArticleCreateView(View):
+    
+    form_class = ArticleCreateForm
+    initials = {'title': 'New Title', 'content': 'New content'}
+    
+    def get(self, request):
+        form = self.form_class(initial=self.initials)
+        return render(request, 'blog/article-create.html', {'form': form})
+    
+    def post(self, request):
+        form = self.form_class(**request.POST)
+        if form.is_valid():
+            article = form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 f"{article.title} created!")
+            return redirect('article_list')
+        
+        messages.add_message(request, messages.ERROR, "Invalid data!")
+        return render(request, 'blog/article-create.html', {'form': form})
