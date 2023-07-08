16f427bef015c194769147f17988c030fec0201c
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-07-08 20:28:55 +0330

    Article model created in blog.models file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Article(models.Model):
+    
+        title = models.CharField(max_length=100)
+        content = models.TextField()
+        active = models.BooleanField(default=True)
+    
+        class Meta:
+            verbose_name = ("article")
+            verbose_name_plural = ("articles")
+    
+        def __str__(self):
+            return self.title
+    
+        def get_absolute_url(self):
+            return reverse("article_detail", kwargs={"pk": self.pk})
+    
