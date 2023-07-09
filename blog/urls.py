fbbab53899b9fed147cd35aa44f8b6dbae9a70c5
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-07-09 14:05:15 +0330

    ArticleDeleteView route added to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleCreateView, ArticleDetailView, ArticleListView
+from .views import ArticleCreateView, ArticleDetailView, ArticleListView, ArticleDeleteView
 
 urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='article_create'),
     path('list/', ArticleListView.as_view(), name='article_list'),
     path('detail/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='article_delete'),
 ]
